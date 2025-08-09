d7b0e269e9382da644faaa4400a6e93070a8ac45
Pol <polcf07@gmail.com>
2025-08-09 22:11:39 +0200

    Agregar archivos de proyecto.

---
+++ Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Monobloc_RanKing
+{
+    public partial class Form1 : Form
+    {
+        //Objects
+
+
+
+
+        //Constructor
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
